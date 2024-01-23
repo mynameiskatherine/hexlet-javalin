5bd5bbaee8e97cb17de14f2711304d94bdb30a15
Kate <e.sheverdenok@gmail.com>
2024-01-23 09:30:57 +0000

    changed main class, added /name

---
+++ src/main/java/org/example/hexlet/App.java
@@ -0,0 +1,22 @@
+package org.example.hexlet;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        return app;
+    }
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(7070);
+    }
+}
